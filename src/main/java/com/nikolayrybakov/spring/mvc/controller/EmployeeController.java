c6d236f864f5086f45acc3f264016f9bdad7fd6c
Nikolay Rybakov <ni_fanta@mail.ru>
2021-03-10 19:18:13 +0300

    create method showAllEmployees in controller

--- src/main/java/com/nikolayrybakov/spring/mvc/controller/EmployeeController.java
+++ src/main/java/com/nikolayrybakov/spring/mvc/controller/EmployeeController.java
@@ -1,13 +1,25 @@
 package com.nikolayrybakov.spring.mvc.controller;
 
+import com.nikolayrybakov.spring.mvc.dao.EmployeeDAO;
+import com.nikolayrybakov.spring.mvc.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
     @RequestMapping("/")
-    public String showAllEmployees(){
+    public String showAllEmployees(Model model){
+        List<Employee> allEmployees = employeeDAO.getAllEmployee();
+        model.addAttribute("employees", allEmployees);
+
         return "allEmployees";
     }
 }
